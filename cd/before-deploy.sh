78b09470bfa5e9586c2438035df9c0a7e4979532
Islam <tariq_islam@intuit.com>
2016-05-10 16:21:46 -0700

    test

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" == 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_58dce6bb7ff5_key -iv $encrypted_58dce6bb7ff5_iv -in codesigning.asc.enc -out codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_58dce6bb7ff5_key -iv $encrypted_58dce6bb7ff5_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
