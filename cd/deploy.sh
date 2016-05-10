4a65229bc4fbe6bac2b70ab020e6a2d34addd624
Islam <tariq_islam@intuit.com>
2016-05-10 16:08:32 -0700

    test

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" == 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign --settings cd/settings.xml
+    mvn deploy -e -X -P sign --settings cd/settings.xml
 fi
