490e90e3ca923638f7febc87d3466cec45dd13bf
jwtodd <james.w.todd@gmail.com>
2016-05-15 12:48:03 -0700

    mod: mv settings.xml

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -2,7 +2,8 @@
 
 if [ "$TRAVIS_BRANCH" == 'develop' ] || [ "$TRAVIS_BRANCH" == 'master' ]; then
   if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_351032695731_key -iv $encrypted_351032695731_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_351032695731_key -iv $encrypted_351032695731_iv \
+      -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     gpg --fast-import .travis/codesigning.asc
   fi
 fi
