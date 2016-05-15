ce5d699b2c6ab22561c632f874e3fdfeb4237a10
James Todd <james@xphylz-2.lan>
2016-05-15 04:56:20 -0700

    mod: deploy typo

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 if [ "$TRAVIS_BRANCH" == 'develop' ] || [ "$TRAVIS_BRANCH" == 'master' ]; then
   if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_351032695731_key -iv $encrypted_351032695731_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
